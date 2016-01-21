5fbf474ccff110a90ed5135c224db187c7b41df2
le-doude <edouard.pelosi@gmail.com>
2016-01-21 18:07:43 +0900

    count lgtms

--- hubnews.rb
+++ hubnews.rb
@@ -1,16 +1,23 @@
 require 'github_api'
 require 'awesome_print'
 
-GITHUB_TOKEN = "dsfukhdsuafhdiuh"
-SLACK_TOKEN = "kajshfdjkshf"
+GITHUB_TOKEN = ENV["GITHUB_TOKEN"]
+SLACK_TOKEN = ENV["SLACK_TOKEN"]
 
 github = Github.new oauth_token: GITHUB_TOKEN
-l = github.pull_requests.list('en-japan', 'waza')
+
+pr_to_lgtm = {}
+
+l = github.pull_requests.list('en-japan', 'waza', state: "open")
 l.each do |pr|
+  lgtms = 0
   if pr[:state] == "open"
     comments = github.pull_requests.comments.list 'en-japan', 'waza', number: pr[:number]
     comments.each do |comment|
-      ap comment
+      lgtms = lgtms + 1 if comment[:body].include?("LGTM")
     end
   end
+  pr_to_lgtm[pr[:number]] = lgtms
 end
+
+ap pr_to_lgtm
